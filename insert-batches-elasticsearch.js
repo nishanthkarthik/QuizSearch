f3603a758bf58f2e4297ac1e8b99a84c55152c23
Magnus Bae <magnusbae@gmail.com>
2014-05-02 04:42:07 +0200

    moved insert functions to own query

---
+++ insert-batches-elasticsearch.js
@@ -0,0 +1,49 @@
+var inserted = 0;
+
+// for(var i = 0; i < fsaeParser.paragraphs.length; i++){
+// 	client.create({
+// 		index:'fsae',
+// 		type:'paragraphs',
+// 		body:fsaeParser.paragraphs[i]
+// 	}, function(error, response){
+// 		if(error){
+// 			console.log('An error occurred inserting ' 
+// 			            + fsaeParser.paragraphs[i] 
+// 			            + '. Error: ' + error);
+// 		}else{
+// 			console.log(++inserted + ' documents inserted');
+// 		}
+// 	});
+// }
+
+// for(var i = 0; i < fseParser.paragraphs.length; i++){
+// 	client.create({
+// 		index:'fse',
+// 		type:'paragraphs',
+// 		body:fseParser.paragraphs[i]
+// 	}, function(error, response){
+// 		if(error){
+// 			console.log('An error occurred inserting ' 
+// 			            + fseParser.paragraphs[i] 
+// 			            + '. Error: ' + error);
+// 		}else{
+// 			console.log(++inserted + ' documents inserted');
+// 		}
+// 	});
+// }
+
+// for(var i = 0; i < eventParser.paragraphs.length; i++){
+// 	client.create({
+// 		index:'event',
+// 		type:'paragraphs',
+// 		body:eventParser.paragraphs[i]
+// 	}, function(error, response){
+// 		if(error){
+// 			console.log('An error occurred inserting ' 
+// 			            + eventParser.paragraphs[i] 
+// 			            + '. Error: ' + error);
+// 		}else{
+// 			console.log(++inserted + ' documents inserted');
+// 		}
+// 	});
+// }
