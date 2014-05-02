6eba1463e83cfdcde516644faa4698aa504b7de7
Magnus Bae <magnusbae@gmail.com>
2014-05-02 06:00:59 +0200

    now searching in all three resultsets

--- app.js
+++ app.js
@@ -18,64 +18,129 @@ fseParser.parseRules();
 eventParser.parseRules();
 
 
-client.ping({
-  requestTimeout: 1000,
-  // undocumented params are appended to the query string
-  hello: "elasticsearch!"
-}, function (error) {
-  if (error) {
-    console.error('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
-  }
+// client.ping({
+//   requestTimeout: 1000,
+//   // undocumented params are appended to the query string
+//   hello: "elasticsearch!"
+// }, function (error) {
+//   if (error) {
+//     console.error('elasticsearch cluster is down!');
+//     exit(1);
+//   } else {
+//     console.log('All is well');
+//   }
+// });
+
+
+var express = require('express');
+var app = express();
+var cors = require('cors');
+
+app.use(cors());
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/search/:query', function(req, res){
+	console.log(req.params.query);
+	client.search({
+		index:'fsae,event,fse',
+		type:'paragraphs',
+		body: {
+			query :{
+				bool:{
+					should:[{
+						query_string:{
+							default_field:'paragraphs.text',
+							query:req.params.query
+						}
+					}]
+				}
+			}
+		}
+	}).then(function(resp){
+		res.json(resp.hits.hits);
+	}, function(err){
+		console.trace(err.message);
+	});
 });
 
-var inserted = 0;
 
-// for(var i = 0; i < fsaeParser.paragraphs.length; i++){
-// 	client.create({
-// 		index:'fsae',
-// 		type:'paragraphs',
-// 		body:fsaeParser.paragraphs[i]
-// 	}, function(error, response){
-// 		if(error){
-// 			console.log('An error occurred inserting ' 
-// 			            + fsaeParser.paragraphs[i] 
-// 			            + '. Error: ' + error);
-// 		}else{
-// 			console.log(++inserted + ' documents inserted');
-// 		}
-// 	});
-// }
+app.get('/fse/:query', function(req, res){
+	console.log(req.params.query);
+	client.search({
+		index:'fse',
+		type:'paragraphs',
+		body: {
+			query :{
+				bool:{
+					should:[{
+						query_string:{
+							default_field:'paragraphs.text',
+							query:req.params.query
+						}
+					}]
+				}
+			}
+		}
+	}).then(function(resp){
+		res.json(resp.hits.hits);
+	}, function(err){
+		console.trace(err.message);
+	});
+});
 
-// for(var i = 0; i < fseParser.paragraphs.length; i++){
-// 	client.create({
-// 		index:'fse',
-// 		type:'paragraphs',
-// 		body:fseParser.paragraphs[i]
-// 	}, function(error, response){
-// 		if(error){
-// 			console.log('An error occurred inserting ' 
-// 			            + fseParser.paragraphs[i] 
-// 			            + '. Error: ' + error);
-// 		}else{
-// 			console.log(++inserted + ' documents inserted');
-// 		}
-// 	});
-// }
+app.get('/fsae/:query', function(req, res){
+	console.log(req.params.query);
+	client.search({
+		index:'fsae',
+		type:'paragraphs',
+		body: {
+			query :{
+				bool:{
+					should:[{
+						query_string:{
+							default_field:'paragraphs.text',
+							query:req.params.query
+						}
+					}]
+				}
+			}
+		}
+	}).then(function(resp){
+		res.json(resp.hits.hits);
+	}, function(err){
+		console.trace(err.message);
+	});
+});
 
-for(var i = 0; i < eventParser.paragraphs.length; i++){
-	client.create({
+app.get('/event/:query', function(req, res){
+	console.log(req.params.query);
+	client.search({
 		index:'event',
 		type:'paragraphs',
-		body:eventParser.paragraphs[i]
-	}, function(error, response){
-		if(error){
-			console.log('An error occurred inserting ' 
-			            + eventParser.paragraphs[i] 
-			            + '. Error: ' + error);
-		}else{
-			console.log(++inserted + ' documents inserted');
+		body: {
+			query :{
+				bool:{
+					should:[{
+						query_string:{
+							default_field:'paragraphs.text',
+							query:req.params.query
+						}
+					}]
+				}
+			}
 		}
+	}).then(function(resp){
+		res.json(resp.hits.hits);
+	}, function(err){
+		console.trace(err.message);
 	});
-}
+});
+
+
+
+app.listen('8080', function(){
+	console.log('listening on 8080');
+})
--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -1,1 +1,69 @@
 console.log('\'Allo \'Allo!');
+
+var $search
+var value;
+var $fse, $fsae, $event;
+
+var host = 'http://localhost:8080';
+
+function search(){
+
+	$.getJSON(host + '/fsae/' + value)
+			.done(function(data){
+				console.log(data);
+				$fsae.append(createHTML(data));
+			})
+			.fail(function(jxqhr, err, something){
+				console.log(arguments);
+			});
+
+	$.getJSON(host + '/fse/' + value)
+			.done(function(data){
+				console.log(data);
+				$fse.append(createHTML(data));
+			})
+			.fail(function(jxqhr, err, something){
+				console.log(arguments);
+			});
+
+	$.getJSON(host + '/event/' + value)
+			.done(function(data){
+				console.log(data);
+				$event.append(createHTML(data));
+			})
+			.fail(function(jxqhr, err, something){
+				console.log(arguments);
+			});
+}
+
+function createHTML(data){
+	var $cont = $('<div/>');
+	
+	for(var i = 0; i < data.length; i++){
+		$cont.append(
+             $('<div/>',{
+				class:'result',
+				html:'<strong>' + data[i]._source.part + '</strong><br/>'
+					+ data[i]._source.text
+			})	
+		);
+	}
+	return $cont;
+}
+
+
+$(function readyFunction () {
+	$search = $('#search');
+
+	$fse = $('#fse');
+	$fsae = $('#fsae');
+	$event = $('#event');
+
+	$search.keydown(function(){
+		value = encodeURIComponent($(this).val());
+
+		$('.resultset').empty();
+		search();
+
+	})
+});
